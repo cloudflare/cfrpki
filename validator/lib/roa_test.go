433b9a587f3f15d3ba43e7fb905ccc7f6dfb2b2d
Nate Sales <nsales@cloudflare.com>
2021-08-18 16:52:39 -0700

    add test for ROA bounds checks

--- validator/lib/roa_test.go
+++ validator/lib/roa_test.go
@@ -72,3 +72,58 @@ func TestEncodeROA(t *testing.T) {
 	_, err = dc.DecodeROA(entriesBytes)
 	assert.Nil(t, err)
 }
+
+func TestValidateROAEntry(t *testing.T) {
+	// Valid
+	_, ipnet, _ := net.ParseCIDR("192.0.2.0/24")
+	roaEntryValid := ROAEntry{
+		IPNet:     ipnet,
+		MaxLength: 24,
+	}
+
+	// Invalid (max length too small)
+	_, ipnet, _ = net.ParseCIDR("192.0.2.0/24")
+	roaEntryInvalidSmallMaxLength := ROAEntry{
+		IPNet:     ipnet,
+		MaxLength: 8,
+	}
+
+	// Invalid IPv4 (max length out of bounds)
+	_, ipnet, _ = net.ParseCIDR("192.0.2.0/24")
+	roaEntryInvalidLargeMaxLength := ROAEntry{
+		IPNet:     ipnet,
+		MaxLength: 128,
+	}
+
+	// Invalid IPv6 (max length out of bounds)
+	_, ipnet, _ = net.ParseCIDR("2001:db8::/128")
+	roaEntryInvalidv6LargeMaxLength := ROAEntry{
+		IPNet:     ipnet,
+		MaxLength: 130,
+	}
+
+	// Invalid (max length negative)
+	_, ipnet, _ = net.ParseCIDR("0.0.0.0/0")
+	roaEntryInvalidNegativeMaxLength := ROAEntry{
+		IPNet:     ipnet,
+		MaxLength: -1,
+	}
+
+	for _, tc := range []struct {
+		ROAEntry    ROAEntry
+		ShouldError bool
+	}{
+		{roaEntryValid, false},
+		{roaEntryInvalidSmallMaxLength, true},
+		{roaEntryInvalidLargeMaxLength, true},
+		{roaEntryInvalidv6LargeMaxLength, true},
+		{roaEntryInvalidNegativeMaxLength, true},
+	} {
+		err := tc.ROAEntry.Validate()
+		if !tc.ShouldError {
+			assert.Nil(t, err)
+		} else {
+			assert.NotNil(t, err)
+		}
+	}
+}
