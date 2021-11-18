15ba623b09ad0d256fc7af59733d9605b994b0d9
Jonathan Senecal <contact@jonathansenecal.com>
2021-11-18 17:17:56 -0500

    Typo fix for after-install-octorpki.sh
    `man adduser` says:
    
    ```
    --disabled-login
              Do not run passwd to set the password.  The user won't be able 
              to use  her  account until the password is set.
    ```

--- package/after-install-octorpki.sh
+++ package/after-install-octorpki.sh
@@ -3,7 +3,7 @@
 set -x
 
 addgroup --system octorpki
-adduser --system --home /var/lib/octorpki --shell /usr/sbin/nologin --disable-login --group octorpki
+adduser --system --home /var/lib/octorpki --shell /usr/sbin/nologin --disabled-login --group octorpki
 
 systemctl daemon-reload
 systemctl enable octorpki.service
