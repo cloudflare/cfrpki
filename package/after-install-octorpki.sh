8133a024139abd8f9d88ab5f3ec489c2864cca49
Oliver Geiselhardt-Herms <ogeiselhardt-herms@cloudflare.com>
2021-11-18 11:29:00 +0100

    VULN-11642: Run octorpki as non-root user

---
+++ package/after-install-octorpki.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -x
+
+addgroup --system octorpki
+adduser --system --home /var/lib/octorpki --shell /usr/sbin/nologin --disable-login --group octorpki
+
+systemctl daemon-reload
+systemctl enable octorpki.service
+systemctl start octorpki
+
+exit 0
